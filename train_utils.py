3871ff0e60ea3a7ee1fc38fea359e3555c1f0c44
ntkhoa95 <toankhoabk@gmail.com>
2022-11-17 19:42:25 +0700

    update train_utils

--- train_utils.py
+++ train_utils.py
@@ -10,9 +10,7 @@ import torch.nn.functional as F
 
 from anomalib.utils.metrics import AUPRO, AUROC
 
-
 _logger = logging.getLogger('train')
-
 
 class AverageMeter:
     """Computes and stores the average and current value"""
@@ -33,41 +31,41 @@ class AverageMeter:
 
 
 
-def training(model, trainloader, validloader, criterion, optimizer, scheduler, num_training_steps: int = 1000, loss_weights: List[float] = [0.6, 0.4], 
-             log_interval: int = 1, eval_interval: int = 1, savedir: str = None, use_wandb: bool = False, device: str ='cpu') -> dict:   
+def training(model, trainloader, validloader, 
+             criterion, optimizer, scheduler, 
+             num_training_steps: int = 1000, 
+             loss_weights: List[float] = [0.6, 0.4], 
+             log_interval: int = 1, 
+             eval_interval: int = 1, 
+             savedir: str = None, use_wandb: bool = False, 
+             device: str ='cpu') -> dict:
 
-    batch_time_m = AverageMeter()
-    data_time_m = AverageMeter()
-    losses_m = AverageMeter()
-    l1_losses_m = AverageMeter()
+    batch_time_m   = AverageMeter()
+    data_time_m    = AverageMeter()
+    losses_m       = AverageMeter()
+    l1_losses_m    = AverageMeter()
     focal_losses_m = AverageMeter()
     
-    # criterion
+    # Criterion
     l1_criterion, focal_criterion = criterion
-    l1_weight, focal_weight = loss_weights
-    
-    # set train mode
-    model.train()
+    l1_weight, focal_weight       = loss_weights
 
-    # set optimizer
-    optimizer.zero_grad()
-
-    # training
     best_score = 0
-    step = 0
-    train_mode = True
-    while train_mode:
-
+    global_step = 0
+    
+    # Begin training
+    for _step in range(num_training_steps):
+        model.train()
         end = time.time()
         for inputs, masks, targets in trainloader:
-            # inputs.shape:     Tensor [N, 3, 256, 256]
-            # masks.shape:      Tensor [N, 256, 256]
-            # targets.shape:    Tensor [N]
             inputs, masks, targets = inputs.to(device), masks.to(device), targets.to(device)
 
             data_time_m.update(time.time() - end)
 
-            # predict
+            # set optimizer
+            optimizer.zero_grad()
+            
+            # Get prediction
             outputs = model(inputs)
             outputs = F.softmax(outputs, dim=1)
             l1_loss = l1_criterion(outputs[:,1,:], masks)
@@ -75,29 +73,26 @@ def training(model, trainloader, validloader, criterion, optimizer, scheduler, n
             loss = (l1_weight * l1_loss) + (focal_weight * focal_loss)
 
             loss.backward()
-            
-            # update weight
-            optimizer.step()
-            optimizer.zero_grad()
 
-            # log loss
+            optimizer.step()
+
+            # Log loss
             l1_losses_m.update(l1_loss.item())
             focal_losses_m.update(focal_loss.item())
             losses_m.update(loss.item())
-            
             batch_time_m.update(time.time() - end)
 
             # wandb
             if use_wandb:
                 wandb.log({
                     'lr':optimizer.param_groups[0]['lr'],
-                    'train_focal_loss':focal_losses_m.val,
-                    'train_l1_loss':l1_losses_m.val,
-                    'train_loss':losses_m.val
+                    'train_focal_loss': focal_losses_m.val,
+                    'train_l1_loss': l1_losses_m.val,
+                    'train_loss': losses_m.val
                 },
-                step=step)
-            
-            if (step+1) % log_interval == 0 or step == 0: 
+                step=_step)
+
+            if (_step+1) % log_interval == 0 or _step == 0: 
                 _logger.info('TRAIN [{:>4d}/{}] '
                             'Loss: {loss.val:>6.4f} ({loss.avg:>6.4f}) '
                             'L1 Loss: {l1_loss.val:>6.4f} ({l1_loss.avg:>6.4f}) '
@@ -105,7 +100,7 @@ def training(model, trainloader, validloader, criterion, optimizer, scheduler, n
                             'LR: {lr:.3e} '
                             'Time: {batch_time.val:.3f}s, {rate:>7.2f}/s ({batch_time.avg:.3f}s, {rate_avg:>7.2f}/s) '
                             'Data: {data_time.val:.3f} ({data_time.avg:.3f})'.format(
-                            step+1, num_training_steps, 
+                            _step+1, num_training_steps, 
                             loss       = losses_m, 
                             l1_loss    = l1_losses_m,
                             focal_loss = focal_losses_m,
@@ -115,69 +110,57 @@ def training(model, trainloader, validloader, criterion, optimizer, scheduler, n
                             rate_avg   = inputs.size(0) / batch_time_m.avg,
                             data_time  = data_time_m))
 
+        # Evaluation
+        if ((_step+1) % eval_interval == 0 and _step != 0) or (_step+1) == num_training_steps:
+            # Eval on validation set
+            eval_metrics = evaluate(model, validloader, criterion, log_interval, device)
+            val_score = np.mean(list(eval_metrics.values()))
+            
+            # scheduler.step(val_score)
 
-            if ((step+1) % eval_interval == 0 and step != 0) or (step+1) == num_training_steps: 
-                eval_metrics = evaluate(model, validloader, criterion, log_interval, device)
-                model.train()
+            eval_log = dict([(f'eval_{k}', v) for k, v in eval_metrics.items()])
 
-                eval_log = dict([(f'eval_{k}', v) for k, v in eval_metrics.items()])
+            # wandb
+            if use_wandb:
+                wandb.log(eval_log, step=_step)
 
-                # wandb
-                if use_wandb:
-                    wandb.log(eval_log, step=step)
+            # checkpoint
+            if best_score < val_score:
+                # save best score
+                state = {'best_step':_step}
+                state.update(eval_log)
+                json.dump(state, open(os.path.join(savedir, 'best_score.json'),'w'), indent='\t')
 
-                # checkpoint
-                if best_score < np.mean(list(eval_metrics.values())):
-                    # save best score
-                    state = {'best_step':step}
-                    state.update(eval_log)
-                    json.dump(state, open(os.path.join(savedir, 'best_score.json'),'w'), indent='\t')
+                # save best model
+                torch.save(model.state_dict(), os.path.join(savedir, f'best_model.pt'))
+                
+                _logger.info('Best Score {0:.3%} to {1:.3%}'.format(best_score, val_score))
 
-                    # save best model
-                    torch.save(model.state_dict(), os.path.join(savedir, f'best_model.pt'))
-                    
-                    _logger.info('Best Score {0:.3%} to {1:.3%}'.format(best_score, np.mean(list(eval_metrics.values()))))
+                best_score = val_score
 
-                    best_score = np.mean(list(eval_metrics.values()))
-
-            # scheduler
-            if scheduler:
-                scheduler.step()
-
-            end = time.time()
-
-            step += 1
-
-            if step == num_training_steps:
-                train_mode = False
-                break
+        scheduler.step()
 
     # print best score and step
-    _logger.info('Best Metric: {0:.4%} (step {1:})'.format(best_score, state['best_step']))
+    _logger.info('Best Metric: {0:.3%} (step {1:})'.format(best_score, state['best_step']))
 
     # save latest model
     torch.save(model.state_dict(), os.path.join(savedir, f'latest_model.pt'))
 
     # save latest score
-    state = {'latest_step':step}
+    state = {'latest_step':_step}
     state.update(eval_log)
     json.dump(state, open(os.path.join(savedir, 'latest_score.json'),'w'), indent='\t')
 
     
-    
-
-        
 def evaluate(model, dataloader, criterion, log_interval, device='cpu'):
+    model.eval()
+    # Evaluation metrics
     auroc_image_metric = AUROC(num_classes=1, pos_label=1)
     auroc_pixel_metric = AUROC(num_classes=1, pos_label=1)
     aupro_pixel_metric = AUPRO()
-
-    model.eval()
     with torch.no_grad():
         for idx, (inputs, masks, targets) in enumerate(dataloader):
             inputs, masks, targets = inputs.to(device), masks.to(device), targets.to(device)
-            
-            # predict
             outputs = model(inputs)
             outputs = F.softmax(outputs, dim=1)
             anomaly_score = torch.topk(torch.flatten(outputs[:,1,:], start_dim=1), 100)[0].mean(dim=1)
@@ -194,8 +177,7 @@ def evaluate(model, dataloader, criterion, log_interval, device='cpu'):
             aupro_pixel_metric.update(
                 preds   = outputs[:,1,:].cpu(),
                 target  = masks.cpu()
-            ) 
-
+            )
     # metrics    
     metrics = {
         'AUROC-image':auroc_image_metric.compute().item(),
@@ -203,8 +185,9 @@ def evaluate(model, dataloader, criterion, log_interval, device='cpu'):
         'AUPRO-pixel':aupro_pixel_metric.compute().item()
 
     }
-
-    _logger.info('TEST: AUROC-image: %.4f%% | AUROC-pixel: %.4f%% | AUPRO-pixel: %.4f%%' % 
-                (metrics['AUROC-image']*100, metrics['AUROC-pixel']*100, metrics['AUPRO-pixel']*100))
+    _logger.info("\n================================")
+    _logger.info('TEST: AUROC-image: %.3f%% | AUROC-pixel: %.3f%% | AUPRO-pixel: %.3f%%' % 
+                (metrics['AUROC-image'] * 100, metrics['AUROC-pixel'] * 100, metrics['AUPRO-pixel'] * 100))
+    _logger.info("\n================================")
 
     return metrics
